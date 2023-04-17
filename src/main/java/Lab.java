46aa26dae14cc6d5c9c2be8bfc1209b6c497ea15
Tamiru Taye <tamirulalan@gmail.com>
2023-04-17 20:34:07 +0000

    array

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,7 +15,7 @@ public class Lab {
      * @return the length of arr.
      */
     public int getLengthOfArray(int[] arr){
-        return 0;
+        return arr.length;
     }
 
 
@@ -27,7 +27,7 @@ public class Lab {
      * @return the element at index n of arr.
      */
     public int getNthElementOfArray(int[] arr, int n){
-        return 0;
+        return arr[n];
     }
 
     /**
@@ -39,14 +39,23 @@ public class Lab {
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
     public void setNthElementOfArray(int[] arr, int n, int val){
+        
+       
+           arr[n]=val; //
+           
+        }
+        
+    
+        
 
-    }
+    
 
     /**
      * @param n the size of the array we wish to create
      * @return a new int array that can contain n elements.
      */
     public int[] returnNewArraySizeN(int n){
-        return null;
+        int arr1[] = new int[n];
+        return arr1;
     }
 }
